d9a9b7465be576d51744014c951cf129e0bde67b
k.Kitada <1127ways@gmail.com>
2015-06-14 16:07:17 +0900

    ほぼ出来上がりw

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,48 @@
+class PostsController < ApplicationController
+
+ def index
+   @posts = Post.all
+ end
+
+ def new
+  @post = Post.new
+ end
+
+ def create
+  @post = Post.new(post_params)
+  @post.save
+  redirect_to "/posts/#{@post.id}"
+ end
+
+ def show
+  @post = Post.find(params[:id])
+ end
+
+ def edit
+ @post = Post.find(params[:id])
+ end
+
+ def update
+ @post = Post.find(params[:id])
+ @post.update_attributes(post_params)
+ redirect_to"/posts/#{@post.id}"
+ end
+
+ def destroy
+  @post = Post.find(params[:id])
+  @post.destroy
+  redirect_to"/posts/"
+ end
+
+private
+
+def post_params
+    params.require(:post).permit(
+      :title,
+      :body,
+      :category,
+)
+end
+
+
+end
